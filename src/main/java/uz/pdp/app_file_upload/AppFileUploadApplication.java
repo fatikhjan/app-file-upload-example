fa72831ae8ab7bb4e037aec50bf398edea86269b
Fotikhjon <muhammad2004fotih@gmail.com>
2023-03-03 14:54:05 +0500

    Initial commit

---
+++ src/main/java/uz/pdp/app_file_upload/AppFileUploadApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.app_file_upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppFileUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppFileUploadApplication.class, args);
+	}
+
+}
